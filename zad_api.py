a9605f11337efc5fcafaa9dae20cdcb46f7fa3a2
Filip Twardawa <i15.twardawa@gmail.com>
2024-04-08 20:11:23 +0200

    Creating an endpoint with movies with the address /movies :

--- zad_api.py
+++ zad_api.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+import csv
 
 app = FastAPI()
+
+
+class Movie:
+    def __init__(self, movie_id, title, genres):
+        self.movie_id = movie_id
+        self.title = title
+        self.genres = genres
 
 
 @app.get("/test")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/movies")
+def get_movies():
+    movies = []
+    with open('movies.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if isinstance(row, dict):
+                movie_id = row.get('movieId')
+                title = row.get('title')
+                genres = row.get('genres')
+                movie = Movie(movie_id, title, genres)
+                movies.append(movie.__dict__)
+    return movies
